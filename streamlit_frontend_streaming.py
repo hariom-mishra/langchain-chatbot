834acf46343a95b1ad63a3e30bcf25bf6236f37b
Hariom Mishra <mhariom014@gmail.com>
2026-03-15 18:55:59 +0530

    added streaming feature to the chatbot

--- streamlit_frontend_streaming.py
+++ streamlit_frontend_streaming.py
@@ -7,11 +7,6 @@ CONFIG = {"configurable": {"thread_id": "thread-1"}}
 if 'message_history' not in st.session_state:
     st.session_state.message_history = []
 
-
-st.sidebar.title("LangGraph ChatBot")
-st.sidebar.button("New Chat")
-st.sidebar.header("My Conversation")
-
 for message in st.session_state.message_history:
     with st.chat_message(message["role"]):
         st.text(message["content"])
@@ -24,9 +19,12 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    initial_state = HumanMessage(content=user_input)
-    response = chatBot.invoke({"messages": [initial_state]}, config=CONFIG)
-    ai_message = response["messages"][-1].content
-    st.session_state.message_history.append({'role': 'assistant', 'content': ai_message})
     with st.chat_message('assistant'):
-        st.text(ai_message)
+        ai_message =st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatBot.stream(
+                {"messages": [HumanMessage(content=user_input)]},
+                config=CONFIG,
+                stream_mode="messages"
+            )
+        )
+        st.session_state.message_history.append({'role': 'assistant', 'content': ai_message})
